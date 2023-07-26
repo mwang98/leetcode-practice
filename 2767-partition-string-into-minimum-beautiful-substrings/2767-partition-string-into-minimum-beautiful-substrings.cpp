b49a018108ad6e501c4ce2c7eeef0819b8fd80cc
Mike Wang <35629082+mwang98@users.noreply.github.com>
2023-07-26 11:33:32 +0800

    Time: 11 ms (19.14%), Space: 6.3 MB (31.22%) - LeetHub

--- 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
@@ -21,10 +21,39 @@ public:
             }
         }
     }
-    int minimumBeautifulSubstrings(string s) {
+    int sol_backtrack(string s) {
         str = s;
         backtrack();
 
         return min_partitions == INIT_PART ? -1 : min_partitions;
+    }
+    int sol_dp(string s) {
+        /* DP
+        Def: dp[i] = the minimum number of substring in such partition of s[:i)
+        Transition:
+            dp[i] = min(dp[i], dp[k] + 1) where s[k+1], ..., s[i] is power of 5
+        TC: O(n^2)
+        SC: O(n)
+        */
+        // Edge case
+        if(s[0] == '0') return -1;
+
+        // Init dp table
+        int dp[16];
+        for(int i = 0 ; i < 16 ; ++i) dp[i] = INIT_PART;
+        dp[0] = 0, dp[1] = 1;
+
+        for(int i = 1 ; i < s.size() ; ++i){
+            for(int k = 0 ; k <= i ; ++k){
+                if(power5.count(s.substr(k, i + 1 - k))){
+                    dp[i + 1] = min(dp[i + 1], dp[k] + 1);
+                }
+            }
+        }
+
+        return dp[s.size()] == INIT_PART ? -1 : dp[s.size()];
+    }
+    int minimumBeautifulSubstrings(string s) {
+        return sol_dp(s);
     }
 };
