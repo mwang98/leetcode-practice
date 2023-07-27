6d3d6fa4dd248e9624188fd7b1dfc168d713b6e0
Mike Wang <35629082+mwang98@users.noreply.github.com>
2023-07-27 10:56:54 +0800

    Time: 234 ms (6.97%), Space: 46.4 MB (5.47%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,40 @@
+enum Type{
+    AB = 0,
+    BA
+};
+
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        s += 'z'; // signal to clear whole stack
+        int gain = 0;
+        vector<char> stack;
+
+        Type greedy = x > y ? Type::AB : Type::BA;
+        int min_point = x > y ? y : x;
+        int max_point = x > y ? x : y;
+
+        for(auto ch: s){
+            // Clear stack
+            if(ch != 'a' && ch != 'b'){
+                vector<char> tmp_stack;
+                for(auto ch: stack){
+                    if(!tmp_stack.empty() && ch != tmp_stack.back()){
+                        gain += min_point;
+                        tmp_stack.pop_back();
+                    } else tmp_stack.push_back(ch);
+                }
+                stack.clear();
+                continue;
+            }
+            // Handle substring starting with `a`, `b`
+            if(stack.empty() || stack.back() == ch) stack.push_back(ch);
+            else if((greedy == Type::AB && ch == 'b') || (greedy == Type::BA && ch == 'a')){
+                gain += max_point;
+                stack.pop_back();
+            } else stack.push_back(ch);
+        }
+
+        return gain;
+    }
+};
