d780b01eaf5b23f9d31b1eee21e8761072a612b5
Mike Wang <35629082+mwang98@users.noreply.github.com>
2023-07-28 11:35:07 +0800

    Time: 101 ms (100.00%), Space: 74.3 MB (16.94%) - LeetHub

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        /* Monotonic stack: increased stack 
+            When to pop: val <= stack.back()
+                Record: the maximum value in stack
+            When to push:
+                Clearing all elements in stack which is larger than val needs `max_val - val` operations
+        */
+        int num_ops = 0;
+        vector<int> stack;
+
+        target.push_back(0); // clear all stack
+
+        for(auto val: target){
+            auto max_val = 0;
+            while(!stack.empty() && val <= stack.back()){ // pop
+                max_val = max(max_val, stack.back());
+                stack.pop_back();
+            }
+
+            if(max_val > 0) num_ops += max_val - val;
+
+            stack.push_back(val); // push
+        }
+
+        return num_ops; // the last element in the stack
+    }
+};
